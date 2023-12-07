a1c1503ba0ac9e7b7a0598ee439e3c25783c2d71
caigx <caisanli_0105@163.com>
2023-12-07 18:44:24 +0800

    feat(*)：解析公式 99%

--- index.js
+++ index.js
@@ -1,115 +1,20 @@
-import { getCompareReg, getParamsByStr, getRefLen, getReg, split, splitComp } from './utils.js';
+import { getParamsByStr, getFuncReg, getNotFuncInfo, getExpressionInfo } from './utils.js';
 import { funcObj } from './func.js';
 
-const oStr = `COUNTIFS(TO_DATE(#row.完结日期_日#) < TO_DATE(#df.转正日期#), #df.判断# == '88VIP', #df.诉求单完成类型# == '诉求单新建完结量')`;
+// const oStr = `COUNTIFS(TO_DATE(#row.完结日期_日#)<TO_DATE(#df.转正日期#), #df.判断#=='88VIP', #df.诉求单完成类型# == '诉求单新建完结量')`;
 // const oStr =  `SUMIFS(#["税收","工资"]#,#df.运营中心# == #row.运营中心#,#data.运营中心# == "重庆") `
 // const oStr =  `SUMIFS(#re__#appoint_date##,“上”,转正日期,It)`
-// const oStr =  `COUNTIFS(#re__#appoint_date##,”上”,转正日期,before)`
+// const oStr =  `COUNTIFS(TODAY()-#re__#appoint_date##,”上”,转正日期,before)`
 // const oStr =  `IF((EOMONTH(TODAY(),-1)- DATE(YEAR(入职日期),MONTH(入职日期),DAY(入职日期)))/30>=12,100,IF((EOMONTH(TODAY(),-1)- DATE(YEAR(入职日期),MONTH(入职日期),DAY(入职日期)))/30>=6,50,0))`
+const oStr =  `IF((EOMONTH(TODAY(),-1)- DATE(YEAR(入职日期),MONTH(入职日期),DAY(入职日期)))/30>=12,100, 20)`;
 
 /**
  * 解析出单个公式的值
  */
 export function parse(str) {
-  const result = getReg().exec(str);
-  if (!result) {
-    console.log('匹配没成功：', str);
-    return getVarVal(str);
-  }
-  // 获取运算符和参数
-  const [, name, params] = result;
-  const func = funcObj[name];
-  if (!func) {
-    console.warn(`未实现：${ name } 方法`);
-    return ;
-  }
-  return func(getParamsByStr(params));
-}
-
-/**
- * 获取表达式的值
- * @param str
- * @returns {*}
- */
-export function getVal(str) {
-  const reg = getRefLen();
-  const execResArr = [];
-  let execRes;
-  let index = -1;
-  console.log('str：', str);
-  // 处理执行函数
-  // while ((execRes = reg.exec(str)) !== null) {
-  //   console.log('execRes[0]：', execRes)
-  //   const val = parse(execRes[0].trim());
-  //   if (index === -1) {
-  //     index = reg.lastIndex;
-  //   } else {
-  //     execResArr.push(execRes.input.slice(index, execRes.index).trim())
-  //     index = reg.lastIndex;
-  //   }
-  //   execResArr.push(val);
-  // }
-  // 处理比较操作符或剩余表达式
-  const lastStr = str.slice(index === -1 ? 0 : index, str.length);
-  console.log('lastStr：', lastStr);
-  if (lastStr) {
-    const compareResArr = splitComp(lastStr);
-    execResArr.push(...compareResArr);
-  }
-  console.log('execResArr：', execResArr);
-  if (!execResArr.length) {
-    // 没有操作函数，就直接取值
-    return parse(str);
-  }
-  const valArr = execResArr.map(str => {
-    return getVarVal(str);
-  })
-  if (valArr.length === 1) {
-    // 仅有一个值
-    return valArr[0];
-  }
-  // 开始获取表达式的值
-  return getArrVal(valArr);
-}
-
-/**
- * 获取变量的值
- * @param str
- */
-function getVarVal(str) {
-  if (str === undefined) {
-    return str;
-  }
-  // 排除变量
-  if (!str.startsWith('#') && !str.startsWith('[')) {
-    return str;
-  }
-  return parseVar(str);
-}
-
-/**
- * 处理变量
- * @param str
- */
-function parseVar(str) {
+  const res = getExpressionInfo(str);
+  console.log('parse result：', res);
   return undefined;
 }
 
-/**
- * 获取运算的值
- * @param arr
- * @returns {*}
- */
-function getArrVal(arr) {
-  // const [first, operator, two] = arr.splice(0, 3);
-  // let res = Function(`return ${ first } ${ operator } ${ two }`)();
-  // if (res.length) {
-  //   arr.unshift(res);
-  //   res = getArrVal(arr);
-  // }
-  // return res;
-  return undefined;
-}
-
-const result = parse(oStr);
-console.log('result：', result);
+parse(oStr);
--- utils.js
+++ utils.js
@@ -1,60 +1,41 @@
-// 比较操作符
 import { funcObj, operation } from './func.js';
 
+// 比较操作符
 export const ComparisonOperators = ['==', '<=', '>=', '!=', '>', '<'];
+// 运算符
+export const Operators = ['/', '+', '-', '*'];
 
 /**
  * 获取比较操作符的正则
  * @returns {RegExp}
  */
-export function getCompareReg() {
-  return new RegExp(`${ ComparisonOperators.join('|') }`, 'g');
+export function getCompareReg(start = false) {
+  return new RegExp(`${ start ? '^' : '' }[${ ComparisonOperators.join('|') }]+`, 'g');
+}
+
+/**
+ * 获取运算符的正则
+ * @returns {RegExp}
+ */
+export function getOperatorReg(start = false) {
+  return new RegExp(`${ start ? '^' : '' }[/|+|\\-|*]+`, 'g');
 }
 
 /**
  * 获取提取操作函数的正则
  * @returns {RegExp}
  */
-export function getReg() {
+export function getFuncReg() {
   return new RegExp(`(${ operation.join('|') })\\((.*)\\)`, 'g');
-}
-
-/**
- * 获取提取操作函数的正则(懒惰模式)
- * @returns {RegExp}
- */
-export function getRefLen() {
-  return new RegExp(`(${ operation.join('|') })\\(([^\\)]*)\\)`, 'g');
-}
-
-/**
- * 按指定逻辑切割并移除空白
- * @param str
- * @param separator
- * @returns {string[]}
- */
-export function split(str, separator = ',') {
-  return str.split(separator).map(str => str.trim())
-}
-const replaceStr = '@1#蔡!%';
-/**
- * 切割比较操作符，并将操作符按顺序插入
- * @param str
- */
-export function splitComp(str) {
-  const reg = getCompareReg();
-  str = str.replace(reg, d => {
-    return replaceStr + d + replaceStr;
-  });
-  return split(str, replaceStr);
 }
 
 /**
  * 获取字符串里的参数
  * @param str
+ * @param comma
  */
-export function getParamsByStr(str) {
-  const comma = ',';
+export function getParamsByStr(str, comma = ',') {
+  // const comma = ',';
   const commaIndexArr = [];
   const keys = ['(', ')', '#', '[', ']'];
   const sideKeyMap = {
@@ -115,7 +96,6 @@ function getConclusionInfo(str) {
       }
     }
   }
-
   return index;
 }
 
@@ -125,37 +105,217 @@ function getConclusionInfo(str) {
  */
 export function getExpressionInfo(str) {
   if (str.startsWith('(')) {
-    const index = getConclusionInfo(str);
     // 获取'('')'里面的表达式
-    const expression = str.slice(1, index);
-    console.log('找到了：', str.slice(1, index));
-    // 继续提取
-    getExpressionInfo(expression);
+    const index = getConclusionInfo(str);
+    const expression = str.substring(1, index);
+    // console.log('找到了：', expression);
+    const res = getExpressionInfo(expression);
+    // 处理剩下的字符串
+    const lastStr = str.substring(index + 1, str.length);
+    const lastRes = getNotFuncInfo(lastStr);
+    return [...res, ...lastRes];
   } else {
-    // 查找函数
-    const reg = new RegExp(`${ operation.join('|') }`, 'g');
-    const res = reg.exec(str);
-    if (res) {
-      const funcName = res[0];
-      const startIndex = res.index;
-      const d = str.substring(reg.lastIndex, str.length);
-      const endIndex = getConclusionInfo(d);
-      const newEndIndex = endIndex + reg.lastIndex + 1;
-      const params = getParamsByStr(str.substring(reg.lastIndex + 1, newEndIndex - 1));
-      // 执行函数
-      const val = funcObj[funcName](params);
-      const lastStr = str.substring(newEndIndex).trim();
-      console.log('执行函数的返回值：', val);
-      console.log('表达式：', newEndIndex, str.substring(startIndex, newEndIndex));
-      console.log('剩下的字符串：', lastStr)
-    } else {
+    const { indexArr, resArr } = getFuncInfo(str);
+    const length = str.length;
+    // console.log('indexArr：', indexArr);
+    // console.log('resArr：', resArr);
+    indexArr.forEach((ii, i) => {
+      let isNext = false;
+      let d = '';
+      if (i === 0) {
+        if (ii[0] !== 0) { // 前面还有表达没处理
+          d = str.substring(0, ii[0]);
+        } else if (ii[1] === length) { // ???
+          // d = str.substring(0, length);
+          console.warn('这是啥情况，注意查收...');
+        } else if (!indexArr[i + 1]) { // 如果第二项不存在
+          d = str.substring(ii[1], length);
+          isNext = true;
+        }
+      } else {
+        d = str.substring(indexArr[i - 1][1], ii[0]);
+      }
+      d = d.trim();
+      if (d !== '') {
+        const res = getNotFuncInfo(d);
+        resArr.splice(isNext ? (i + 1) : i, 0, ...(res || []))
+      }
+    })
 
+    if (!indexArr.length) {
+      resArr.push(...getNotFuncInfo(str));
+    }
+    console.log('resArr：', resArr);
+    return resArr;
+  }
+}
+
+/**
+ * 获取非函数的表达式信息
+ * @param str
+ */
+export function getNotFuncInfo(str) {
+  const arr = [];
+  const length = str.length;
+  // 校验开头是否是比较运算符
+  const comReg = getCompareReg(true);
+  const comRes = comReg.exec(str);
+  if (comRes) {
+    arr.push({
+      type: 'com',
+      val: str.substring(comRes.index, comReg.lastIndex)
+    })
+    const lastStr = str.substring(comReg.lastIndex, length).trim();
+    arr.push(...getNotFuncInfo(lastStr))
+    return arr;
+  }
+
+  // 校验开头是否是运算符
+  const opeReg = getOperatorReg(true);
+  const opeRes = opeReg.exec(str);
+  if (opeRes) {
+    arr.push({
+      type: 'ope',
+      val: str.substring(opeRes.index, opeReg.lastIndex)
+    })
+    const lastStr = str.substring(opeReg.lastIndex, length).trim();
+    arr.push(...getNotFuncInfo(lastStr))
+    return arr;
+  }
+
+  // 检验开头是否是变量
+  if (str.startsWith('#')) {
+    const varIndex = getVarInfo(str);
+    if (varIndex === length - 1) {
+      arr.push({ type: 'var', val: str });
+    } else {
+      const lastStr = str.substring(varIndex, length);
+      arr.push(
+        { type: 'var', val: str.substring(0, varIndex) },
+        ...getNotFuncInfo(lastStr)
+      )
+    }
+    return arr;
+  }
+
+  const comReg2 = getCompareReg();
+  const opeReg2 = getOperatorReg();
+  if (comReg2.test(str) || opeReg2.test(str)) {
+    const { resArr, indexArr } = getOpeInfo(str);
+    indexArr.forEach((ii, i) => {
+      let item;
+      if (i === 0) {
+        item = {
+          val: str.substring(0, ii[0]),
+          type: 'str'
+        }
+      } else {
+        item = {
+          val: str.substring(indexArr[i - 1][1], ii[0]),
+          type: 'str'
+        }
+      }
+      resArr.splice(i, 0, item);
+    })
+    // 说明还没处理完
+    const last = indexArr[indexArr.length - 1];
+    if (last[1] < str.length - 1) {
+      resArr.push({ type: 'str', val: str.substring(last[1]) });
+    }
+    return resArr;
+  }
+
+  return [{ type: 'str', val: str }];
+}
+
+/**
+ * 获取操作符信息
+ */
+function getOpeInfo(str) {
+  const opes = [...ComparisonOperators, ...Operators];
+  const reg = new RegExp(`[${ opes.join('|') }]+`, 'g');
+  let execRes;
+  const resArr = [];
+  const indexArr = [];
+  while ((execRes = reg.exec(str)) !== null) {
+    const val = execRes[0];
+    resArr.push({
+      type: Operators.includes(val) ? 'ope' : 'com',
+      val
+    })
+    indexArr.push([execRes.index, reg.lastIndex])
+  }
+  return { resArr, indexArr }
+}
+
+/**
+ * 获取变量信息
+ * @param str
+ */
+function getVarInfo(str) {
+  const key = '#';
+  let count = 0;
+  let index = -1;
+  for (let i = 0; i < str.length; i++) {
+    const s = str[i];
+    if (s === key) {
+      count++;
+    } else if ([...Operators].includes(s)) {
+      if (count % 2 === 0) {
+        index = i;
+        break;
+      }
+    } else if (
+      ComparisonOperators.map(str => str.substring(0, 1)).includes(s)
+    ) {
+      if (count % 2 === 0) {
+        index = i;
+        break;
+      }
     }
   }
-  return null;
+  return index === -1 ? str.length - 1 : index;
 }
 
-export function parseExpression(str) {
-  getExpressionInfo(str);
-  return undefined;
+/**
+ * 提取函数信息
+ * @param str
+ */
+function getFuncInfo(str) {
+  const reg = new RegExp(`${ operation.join('|') }`, 'g');
+  let execRes;
+  const indexArr = [];
+  const resArr = [];
+  let prevLastIndex = -1;
+  while ((execRes = reg.exec(str)) !== null) {
+    const funcName = execRes[0];
+    const startIndex = execRes.index;
+    // 如果上一次lastIndex大于这次的index，就说明冲突了
+    if (prevLastIndex !== -1 && prevLastIndex > startIndex) {
+      continue;
+    }
+    const d = str.substring(reg.lastIndex, str.length);
+    const endIndex = getConclusionInfo(d);
+    const newEndIndex = endIndex + reg.lastIndex + 1;
+    const params = getParamsByStr(str.substring(reg.lastIndex + 1, newEndIndex - 1));
+    prevLastIndex = newEndIndex;
+    // reg.lastIndex = execRes.index;
+    // 执行函数
+    const val = funcObj[funcName](params);
+    indexArr.push([startIndex, newEndIndex]);
+    resArr.push({ val: val, type: 'str' });
+  }
+  return { indexArr,  resArr }
 }
+
+/**
+ * 解析表达式
+ * @param str
+ * @returns {*|*[]}
+ */
+export function parseExpression(str = '') {
+  const arr = getExpressionInfo(str);
+  return arr;
+}
+
+// console.log('getFuncInfo：', getFuncInfo('EOMONTH(TODAY(),-1)- DATE(YEAR(入职日期),MONTH(入职日期),DAY(入职日期)'));
